b0c66354ed638d1eff33cd1784caf31fa77b4865
jiangwenlong <jiangwenlong.js@chinatelecom.cn>
2024-11-14 23:20:43 +0800

    修改端口

--- app/main.py
+++ app/main.py
@@ -9,4 +9,4 @@ def hello_world():
     return '欢迎使用微信云托管 哈哈2'
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80321)))
