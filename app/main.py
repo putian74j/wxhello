610e7692f824845da19d1f178fa4c99bb2706304
jiangwenlong <jiangwenlong.js@chinatelecom.cn>
2024-11-14 23:28:48 +0800

    哈哈4端口

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '欢迎使用微信云托管 哈哈3'
+    return '欢迎使用微信云托管 哈哈4'
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80321)))
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8032)))
