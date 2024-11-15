3997348a0a3968946978cdd4f71320dfb1962e9d
jiangwenlong <jiangwenlong.js@chinatelecom.cn>
2024-11-15 23:20:54 +0800

    environ1

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '欢迎使用微信云托管 哈哈5'
+    return f'欢迎使用微信云托管 哈哈666:  {os.environ.get('MYSQL_USERNAME')}'
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8032)))
